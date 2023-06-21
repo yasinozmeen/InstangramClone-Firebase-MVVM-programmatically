6b37b170898abe1e13dba450e137064e141ff1d1
Yasin Özmen <yasinozmeen3@gmail.com>
2023-06-21 13:31:13 +0300

    projecet configure finished

---
+++ InstangramClone/InstangramClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstangramClone
+//
+//  Created by Kadir Yasin Özmen on 21.06.2023.
+//
+
+import UIKit.UITabBar
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
